dc5ab481be017252c43d9a728f3f2d4c58cde915
varadhbhatnagar <varadhbhatnagar@gmail.com>
2020-01-06 19:23:23 +0530

    Added OCRHindi

---
+++ OCRHindi/test.py
@@ -0,0 +1,23 @@
+from PIL import Image 
+import pytesseract 
+import cv2
+import numpy as np
+
+
+def remove_noise_and_smooth(file_name):
+    img = cv2.imread(file_name, 0)
+    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
+    kernel = np.ones((1, 1), np.uint8)
+    opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    img = image_smoothening(img)
+    or_image = cv2.bitwise_or(img, closing)
+    return or_image
+
+
+
+filename ='e.jpg'
+fn = remove_noise_and_smooth(filename)
+
+text = str(((pytesseract.image_to_string(Image.open(fn),lang="hin"))))
+print(text) 
